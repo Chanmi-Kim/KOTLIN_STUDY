66c663ada2d3e778df069652ebee7289dace495b
Chanmi Kim <chanmi_dev@naver.com>
2019-08-17 23:36:51 +0900

    add file ex01.kt
    - Chapter 1~2 내용 추가
    - ~p.28

---
+++ 빅 너드 랜치의 코틀린 프로그래밍/ex01.kt
@@ -0,0 +1,8 @@
+fun main(args: Array <String>) {
+    val playerName: String = "에스트라곤"
+    // playerName = "마드리갈"
+    var experiencePoints: Int = 5
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+}
